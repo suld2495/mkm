27a9d961bbab06590f71e0731a9a8001bc66f794
suld2495 <suld7777@gmail.com>
2023-08-02 07:41:49 +0900

    중간 커밋

---
+++ src/components/intro/Section01.jsx
@@ -0,0 +1,32 @@
+const Section01 = () => (
+  <section className="section01">
+    <div className="center">
+      <div className="left">
+        <p>MKM fulfillment</p>
+        <h2>
+          <strong>
+            <span>성장하는 기업의 성공적인 물류</span>
+            엠케이엠 풀필먼트
+          </strong>
+        </h2>
+        <div>
+          <p><strong>당신의 성장 그래프, 빠르고 바르게 나아갑니다.</strong></p>
+          <p>
+            당신의 비즈니스가 우상향 할 수 있도록 엠케이엠 풀필먼트는 <strong>속도의 가치에<br/>
+            집중</strong>합니다. 물류 혁신에 대한 임직원의 사명감과 <strong>완벽에 가까운 업무 철칙</strong>은<br/>
+            단순한 물류 서비스를 넘어 성공적인 사업 파트너가 되기 위한 우리의 철학입니다.
+          </p>
+          <p>
+            임직원이 사명감과 자부심으로 모두의 성고을 완성하는 풀필먼트 플랫폼, MKM <br/>
+            <strong>빠르고, 바르게. 우리가 집중하는 성장의 가치</strong>입니다. 
+          </p>
+        </div>
+      </div>
+      <div className="img">
+        <img src="/images/intro/company01.png" />
+      </div>
+    </div>
+  </section>
+)
+
+export default Section01;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,15 @@
 import type { RouteObject } from 'react-router-dom';
-import Main from '../pages/Main';
+import Main from '../pages/Main.jsx';
+import Intro from '../pages/Intro.jsx';
 
 const routes: RouteObject[] = [
   {
     path: '/',
     element: <Main />,
+  },
+  {
+    path: '/intro',
+    element: <Intro />,
   },
 ];
 
