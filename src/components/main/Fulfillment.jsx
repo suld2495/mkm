d8e64cdd4514cf7476075922bd930aa13d53cc65
suld2495 <suld7777@gmail.com>
2023-08-04 15:16:11 +0900

    애니메이션 적용

--- src/components/main/Fulfillment.jsx
+++ src/components/main/Fulfillment.jsx
@@ -9,11 +9,11 @@ const Fulfillment = () => (
         </h2>
       </div>
       <div className="talk">
-        <div><img src="/images/main/talk01.png" /></div>
-        <div><img src="/images/main/talk02.png" /></div>
-        <div><img src="/images/main/talk03.png" /></div>
-        <div><img src="/images/main/talk04.png" /></div>
-        <div><img src="/images/main/talk05.png" /></div>
+        <div className="wow fadeInLeft" data-wow-duration="1.5s" data-wow-delay=""><img src="/images/main/talk01.png" /></div>
+        <div className="wow fadeInRight" data-wow-duration="1.5s" data-wow-delay="0.3s"><img src="/images/main/talk02.png" /></div>
+        <div className="wow fadeInLeft" data-wow-duration="1.5s" data-wow-delay="0.6s"><img src="/images/main/talk03.png" /></div>
+        <div className="wow fadeInRight" data-wow-duration="1.5s" data-wow-delay="0.9s"><img src="/images/main/talk04.png" /></div>
+        <div className="wow fadeInLeft" data-wow-duration="1.5s" data-wow-delay="1.2s"><img src="/images/main/talk05.png" /></div>
       </div>
     </div>
   </section>
